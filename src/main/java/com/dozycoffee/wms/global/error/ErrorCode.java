f898e915a286c7e9a189f99ca3652a19c8c1e6f9
jinwoo <jjw6654@gmail.com>
2026-08-21 22:18:04 +0900

    chore(global): 전역 예외 처리 추가 (ErrorCode, BusinessException, GlobalExceptionHandler)

---
+++ src/main/java/com/dozycoffee/wms/global/error/ErrorCode.java
@@ -0,0 +1,9 @@
+package com.dozycoffee.wms.global.error;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+    String getCode();
+    String getMessage();
+    HttpStatus getHttpStatus();
+}
---
+++ src/main/java/com/dozycoffee/wms/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.dozycoffee.wms.global.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponseDto> handleBusinessException(BusinessException e) {
+        log.warn("Business exception: code={}, message={}", e.getErrorCode().getCode(), e.getMessage());
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(ErrorResponseDto.of(errorCode));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<ErrorResponseDto> handleValidationException(WebExchangeBindException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponseDto(CommonErrorCode.INVALID_INPUT.getCode(), message, LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
+        log.error("Unhandled exception", e);
+        return ResponseEntity.internalServerError().body(ErrorResponseDto.of(CommonErrorCode.INTERNAL_SERVER_ERROR));
+    }
+}
